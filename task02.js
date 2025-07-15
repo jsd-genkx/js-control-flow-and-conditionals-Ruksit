eb3b06286e4842734a248c2ead5f6a4d9fc0d70a
Ruksit <ruksitm@gmail.com>
2025-07-15 11:47:23 +0700

    task02 solution

--- task02.js
+++ task02.js
@@ -1,7 +1,17 @@
 "use strict";
 
+//const { use } = require("react");
+
 // TODO: Use if/else to check if it's a number
 // (hint: typeof or isNaN)
 
-const userInput = "42";
+const userInput = 55;
 // Try changing to "hello", 10, null, NaN
+
+if (typeof userInput === "number" && !isNaN(userInput)) {
+  console.log("It is a number.");
+} else {
+  console.log("It is NOT a number.");
+}
+
+//-m "task02 solution"
