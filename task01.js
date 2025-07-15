67d1266b121519ed3b34834fc3345fd3e6c35040
Ruksit <ruksitm@gmail.com>
2025-07-15 11:43:41 +0700

    task01 solution

--- task01.js
+++ task01.js
@@ -2,4 +2,12 @@
 
 // TODO: Use if/else to print "Even" or "Odd"
 
-const number = 15;
+const number = 14;
+
+if (number % 2 === 0) {
+    console.log("Your Number is Even");
+}else{
+    console.log("Your Number is Odd");
+}
+
+//-m "task01 solution"
